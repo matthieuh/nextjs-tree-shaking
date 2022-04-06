5d2559a11047e454585d1f8393f3ee19b0a8533f
Matthieu Hostache <matthieu.hostache@gmail.com>
2022-04-06 12:02:29 +0200

    still working

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,18 @@
 export { ShouldBeIncluded } from './components/ShouldBeIncluded';
 export { MustNotBeIncluded } from './components/MustNotBeIncluded';
 export { Provider } from './components/Provider';
+
+// Stitches
+export {
+  styled,
+  css,
+  createTheme,
+  getCssText,
+  globalCss,
+  keyframes,
+  config,
+  lightTheme,
+  darkTheme,
+} from './stitches.config';
+
+export type { CSS, VariantProps } from './stitches.config';
