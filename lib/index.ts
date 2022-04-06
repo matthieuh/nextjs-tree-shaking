756ca320103f270a251076acd175aa99c9fc6398
Matthieu Hostache <matthieu.hostache@gmail.com>
2022-04-06 11:34:58 +0200

    still working

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,3 @@
 export { ShouldBeIncluded } from './components/ShouldBeIncluded/ShouldBeIncluded';
 export { MustNotBeIncluded } from './components/MustNotBeIncluded/MustNotBeIncluded';
+export { Provider } from './components/Provider/Provider';
--- nextjs-app/pages/index.tsx
+++ nextjs-app/pages/index.tsx
@@ -1,10 +1,12 @@
 import { ShouldBeIncluded } from 'lib';
+// import { Button } from '@traefiklabs/faency';
 
 export default function Home() {
   return (
     <div>
       HELLO
       <ShouldBeIncluded />
+      {/* <Button>test</Button> */}
     </div>
   );
 }
